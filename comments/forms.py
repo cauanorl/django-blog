6b42c6fff2aacca05bce2e416d88298aa2093868
Cauanorl <rodriguescauan701@gmail.com>
2022-03-17 13:27:09 -0300

    finalizando o recaptcha nos comentarios, para evitar spans

--- comments/forms.py
+++ comments/forms.py
@@ -1,13 +1,34 @@
 from django.forms import ModelForm
 from .models import Comment
-from django.contrib import messages
+
+import requests
 
 
 class CommentForm(ModelForm):
     def clean(self):
-        data = self.cleaned_data
-        name = data.get('name_comment')
-        comment = data.get('comment')
+        cleaned_data = self.cleaned_data
+        name = cleaned_data.get('name_comment')
+        comment = cleaned_data.get('comment')
+
+        g_recaptcha_response = self.data.get('g-recaptcha-response')
+        secret_recaptcha_key = '6Le4geseAAAAAPNb9nckCONXQHCnuUTJNPhvonCt'
+
+        # https://www.google.com/recaptcha/api/siteverify
+        recaptcha_request = requests.post(
+            url='https://www.google.com/recaptcha/api/siteverify',
+            data={
+                'secret': secret_recaptcha_key,
+                'response': g_recaptcha_response,
+            },
+        )
+
+        recaptcha_result = recaptcha_request.json()
+
+        if not recaptcha_result.get('success'):
+            self.add_error(
+                'comment',
+                'Please check the "I am not a robot" box.',
+            )
 
         if not name:
             self.add_error(
